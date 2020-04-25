7a2ddf710259d15c79bb9d9ddc3dbd5b050780dc
darshan17 <darshansatra1@gmail.com>
2020-04-25 21:49:06 +0530

    structuring on the basis of the platform

--- lib/src/myApp.dart
+++ lib/src/myApp.dart
@@ -1,13 +1,32 @@
 import 'package:farmarket/src/routes.dart';
 import 'package:farmarket/src/screen/login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: Routes.materialRoute,
-      home: Login(),
-    );
+    return PlatformApp();
   }
+}
+
+class PlatformApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    if(Platform.isIOS){
+      return CupertinoApp(
+        onGenerateRoute: Routes.cupertinoRoute,
+        home: Login(),
+      );
+    }else{
+      return MaterialApp(
+        onGenerateRoute: Routes.materialRoute,
+        home: Login(),
+      );
+    }
+    
+  }
+
 }
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,6 +1,7 @@
 import 'package:farmarket/src/screen/landing.dart';
 import 'package:farmarket/src/screen/login.dart';
 import 'package:farmarket/src/screen/signup.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 abstract class Routes{
@@ -20,6 +21,26 @@ abstract class Routes{
 
       default:
       return MaterialPageRoute(builder: (context) => Login());
+      break;
+    }
+  }
+
+  static CupertinoPageRoute cupertinoRoute(RouteSettings settings){
+    switch(settings.name){
+      case "/":
+      return CupertinoPageRoute(builder: (context) => Landing());
+      break;
+
+      case "/signup":
+      return CupertinoPageRoute(builder: (context)=>Signup());
+      break;
+
+      case "/login":
+      return CupertinoPageRoute(builder: (context) => Login());
+      break;
+
+      default:
+      return CupertinoPageRoute(builder: (context) => Login());
       break;
     }
   }
